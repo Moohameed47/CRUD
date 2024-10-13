7319e8309da6d647d0a638f333f439cad086720b
RenadEssam <d.mohamedtarek12@gmail.com>
2024-10-14 01:32:19 +0300

    Link Bootstrap5 & Css  and the make the structure of Card

---
+++ dishes/index.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once "../shared/connection.php";
+require_once "../shared/header.php";
+?>
+
+<section class="">
+    <div class="row">
+        <div class="col-md-4 col-sm-6 col-xs-12">
+            <div class="card border-0 rounded-2">
+                <div class="cover item-a">
+                    <h1>Little<br>Bonsai</h1>
+                    <span class="price">$79</span>
+                    <div class="card-back">
+                        <a href="#">Add to cart</a>
+                        <a href="#">View detail</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php require_once "../shared/footer.php" ?>
--- shared/connection.php
+++ shared/connection.php
@@ -7,7 +7,7 @@ $dbname = "restaurant";
 
 try {
     $connect = mysqli_connect($host, $user, $password, $dbname);
-    echo "Connect";
+    // echo "Connect";
 } catch (Throwable $th) {
     echo "Error: " . $th->getMessage() . "";
 }
